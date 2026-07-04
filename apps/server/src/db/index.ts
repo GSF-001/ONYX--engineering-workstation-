77af5727a0ab72b8f53aebe4b2ffe70119d5a839
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 03:35:28 +0700

    Create index.ts

---
+++ apps/server/src/db/index.ts
@@ -0,0 +1,5 @@
+export * from "./client.js";
+export * from "./schema.js";
+export * from "./queries.js";
+export * from "./transaction.js";
+export { runMigrations, closeDbConnection } from "./migrations.js";
