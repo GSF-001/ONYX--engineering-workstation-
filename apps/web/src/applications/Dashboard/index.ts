2cc21242194d440791b22a4b7d2bfc68761f9c18
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 18:19:11 +0700

    Create index.ts

---
+++ apps/web/src/applications/Dashboard/index.ts
@@ -0,0 +1,6 @@
+export { default } from "./DashboardApp";
+export { DashboardWindow } from "./DashboardWindow";
+export { useDashboardData } from "./DashboardHooks";
+export { DashboardAPI } from "./DashboardAPI";
+export { getActiveRepositoryId, setActiveRepositoryId } from "./DashboardStore";
+export type { DashboardViewState, DashboardPrTrendPoint } from "./DashboardTypes";
