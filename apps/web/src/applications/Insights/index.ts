280cd4b4e81580dbe70c948774132ee164b42239
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 18:52:53 +0700

    Create index.ts

---
+++ apps/web/src/applications/Insights/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./InsightsApp";
+export { InsightsWindow } from "./InsightsWindow";
+export { useInsightsData } from "./InsightsHooks";
+export { InsightsAPI } from "./InsightsAPI";
+export type { InsightsViewState } from "./InsightsTypes";
