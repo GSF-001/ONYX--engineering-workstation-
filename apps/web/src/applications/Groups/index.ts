14ac69b3996c5979411c3ee81a3f1aa75ca8a70c
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-06 04:42:57 +0700

    Create index.ts

---
+++ apps/web/src/applications/Groups/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./GroupsApp";
+export { GroupsWindow } from "./GroupsWindow";
+export { useGroupsData } from "./GroupsHooks";
+export { GroupsAPI } from "./GroupsAPI";
+export type { GroupsViewState, GroupSummary, GroupMember, ChatMessage } from "./GroupsTypes";
