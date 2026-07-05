7f552d18476f5796174aa21ad9127c4b0711993e
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:20:35 +0700

    Create TerminalTypes.ts

---
+++ apps/web/src/applications/Terminal/TerminalTypes.ts
@@ -0,0 +1,3 @@
+export type { ConsoleLine } from "../../terminal/Console";
+export type { ParsedCommand } from "../../terminal/CommandParser";
+export type { TerminalContext, CommandHandler } from "../../terminal/CommandRegistry";
