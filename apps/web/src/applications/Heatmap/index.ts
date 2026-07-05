6b460e3d41317d2defc4908f92cf14c6c159ae09
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:15:44 +0700

    Create index.ts

---
+++ apps/web/src/applications/Heatmap/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./HeatmapApp";
+export { HeatmapWindow } from "./HeatmapWindow";
+export { useHeatmapData } from "./HeatmapHooks";
+export { HeatmapAPI } from "./HeatmapAPI";
+export type { HeatmapViewState } from "./HeatmapTypes";
