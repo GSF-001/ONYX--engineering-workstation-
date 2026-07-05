7b415631bd42e5a0271629ab95f3d47109a5ab57
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-06 04:33:43 +0700

    Create index.ts

---
+++ apps/web/src/applications/Community/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./CommunityApp";
+export { CommunityWindow } from "./CommunityWindow";
+export { CommunityAPI } from "./CommunityAPI";
+export type { CommunityViewState, FeedPost, DeveloperSummary, LeaderboardEntry } from "./CommunityTypes";
