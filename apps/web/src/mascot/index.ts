da3527b36d635bcacfec7b54a0ce990c726494c3
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 18:12:24 +0700

    Create index.ts

---
+++ apps/web/src/mascot/index.ts
@@ -0,0 +1,14 @@
+export { PixComputer } from "./PixComputer";
+export { HappyFace } from "./Happy";
+export { WarningFace } from "./Warning";
+export { ErrorFace } from "./Error";
+export { IdleFace } from "./Idle";
+export { SleepingFace } from "./Sleeping";
+export { TalkingFace } from "./Talking";
+export { EXPRESSIONS, type MascotExpression } from "./Expressions";
+export {
+  expressionForScore,
+  expressionForIdleTime,
+  expressionForError,
+  expressionForSpeaking,
+} from "./Reactions";
