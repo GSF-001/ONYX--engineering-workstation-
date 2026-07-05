42fc7c848b479406473f32a4420c7cc5a953be87
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:11:18 +0700

    Create index.ts

---
+++ apps/web/src/applications/Reports/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./ReportsApp";
+export { ReportsWindow } from "./ReportsWindow";
+export { useReportsData } from "./ReportsHooks";
+export { ReportsAPI } from "./ReportsAPI";
+export type { ReportsViewState, ReportJobRecord, ReportPeriod, ReportFormat } from "./ReportsTypes";
