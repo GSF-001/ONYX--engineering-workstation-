1d7edbc92ab1cefd8021bdf917326961c2f5e39e
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 18:46:35 +0700

    Create index.ts

---
+++ apps/web/src/applications/Issues/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./IssuesApp";
+export { IssuesWindow } from "./IssuesWindow";
+export { useIssuesData } from "./IssuesHooks";
+export { IssuesAPI } from "./IssuesAPI";
+export type { IssuesViewState } from "./IssuesTypes";
