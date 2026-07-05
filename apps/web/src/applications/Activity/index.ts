0c3ce4ebdf67eb9737d8f804400ea98a1740dbbb
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:20:06 +0700

    Create index.ts

---
+++ apps/web/src/applications/Activity/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./ActivityApp";
+export { ActivityWindow } from "./ActivityWindow";
+export { useActivityData } from "./ActivityHooks";
+export { ActivityAPI } from "./ActivityAPI";
+export type { ActivityViewState, EventTypeFilter } from "./ActivityTypes";
