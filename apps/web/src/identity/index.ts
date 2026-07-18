30b3aec3543d4cfb480e96566a9276e11809ee48
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-18 07:15:10 +0700

    Create index.ts

---
+++ apps/web/src/identity/index.ts
@@ -0,0 +1,9 @@
+export { IdentityPicker } from "./IdentityPicker";
+export { IdentityPreview } from "./IdentityPreview";
+export { GenerateIdentityButton } from "./GenerateIdentityButton";
+export { ConfirmIdentity } from "./ConfirmIdentity";
+export { formatCooldownRemaining } from "./IdentityCooldown";
+export { getCachedHandle, setCachedHandle, clearCachedHandle } from "./IdentityStore";
+export { generateAvatarSpec, type AvatarSpec } from "./IdentityAvatar";
+export { IdentityAPI } from "./IdentityAPI";
+export type { IdentityData, CooldownStatus } from "./IdentityTypes";
